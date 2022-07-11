8d2ce7730d944c84c2453628efafc55c101390b9
deibo <deib0@foxmail.com>
2022-07-11 19:31:08 +0800

    完成Money的UI样式

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import Nav from "components/Nav";
+import Nav from './Nav';
+import React from 'react';
 import styled from 'styled-components';
-const Wrapper=styled.div`
-    height:100vh;
-    display: flex;
-    flex-direction:column;
-    justify-content:end;
-    .children-wrapper{
-        flex-grow:1;
-    }
-    
-    `
-function Layout(props:any){
-return (
-        <Wrapper>
-        <div className="children-wrapper">
-            {props.children}
-        </div>
-        <Nav/>
-        </Wrapper>
-)
-}
-export default Layout
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Layout = (props: any) => {
+  return (
+    <Wrapper>
+      <Main className={props.className}>
+        {props.children}
+      </Main>
+      <Nav/>
+    </Wrapper>
+  );
+};
+
+export default Layout;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,65 +1,63 @@
-import styled from "styled-components";
-import { NavLink } from "react-router-dom";
-import React from "react";
-import Icon from "./Icons";
+import styled from 'styled-components';
+import { NavLink} from 'react-router-dom';
+import React from 'react';
+import Icon from 'components/Icons';
 
 const NavWrapper = styled.nav`
+  background: white;
   line-height: 24px;
-  box-shadow:0 0 3px rgba(0,0,0,.25);
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
     display:flex;
     > li{
       width: 33.3333%;
       text-align:center;
-      display: flex;
-      flex-direction: column;
-      padding: 4px 0;
-      justify-content: center;
-      align-items: center;
-      a {
+      > a {
         display: flex;
         flex-direction: column;
-        justify-content:center;
+        padding: 4px 0;
+        justify-content: center;
         align-items: center;
-          .icon {
+        .icon {
           width: 24px;      
           height: 24px;
         }
         &.selected{
-          color:red;
-            .icon {
-              fill: red;
-            }
+          color: red;
+          .icon{
+            fill: red;
+          }
         }
       }
     }
   }
 `;
-function Nav(){
-    return (
-        <NavWrapper>
-            <ul>
-            <li>
-              <NavLink activeClassName="selected" to="/tags">
-              <Icon name="labels" />
-                标签页
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/money" activeClassName="selected">
-              <Icon name="money" />
-                记账页
-                </NavLink>
-            </li>
-            <li>
-              <NavLink to="/statistics" activeClassName="selected">
-              <Icon name="account" />
-                统计页
-              </NavLink>
-            </li>
-                </ul>
-        </NavWrapper>
-    )
-}
-export default Nav
 
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <NavLink to="/tags" activeClassName="selected">
+            <Icon name="labels"/>
+            标签页
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/money" activeClassName="selected">
+            <Icon name="money"/>
+            记账页
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/statistics" activeClassName="selected">
+            <Icon name="account"/>
+            统计页
+          </NavLink>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
