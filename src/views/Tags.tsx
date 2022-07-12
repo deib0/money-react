caad4844090c86a11ca4f60af0e74523cd8eca42
deibo <deib0@foxmail.com>
2022-07-12 21:18:29 +0800

    完成Tags的样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,16 +1,62 @@
 import React from "react";
 import Layout from 'components/Layout';
 import { useTags } from "useTags";
+import styled from 'styled-components';
+import Icon from "components/Icons";
+
+const TagList = styled.ol`
+  font-size: 16px; 
+  background:white;
+  > li{
+    //#e5e5e7
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    .oneLine{
+      width:100%;
+    }
+    
+  }
+`;
+const Button = styled.button`
+  font-size: 18px; border: none; padding: 8px 12px;
+  background: #f60; border-radius: 4px;
+  color: white;
+`;
+const Center = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`
 
 function Tags() {
-    let {tags,setTags} = useTags()
-    return (
+  const {tags} = useTags();
+  return (
     <Layout>
-        <ul>
-                {tags.map(item=><li key={item}>{item}</li>)}
-        </ul>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <p className="oneLine">{tag}</p>
+            <Icon name="right"/>
+          </li>
+        )}
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
-    )
-  }
+  );
+}
 
-  export default Tags
+export default Tags;
